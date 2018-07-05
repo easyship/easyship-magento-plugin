8ca5641fd0bd56d8b53a5bb5db7988d273df2946
Aloha <y.alohac@gmail.com>
2018-07-05 15:11:26 +0800

    dimension_units_to_orders: bump up to 0.1.5

--- app/code/local/Easyship/Shipping/Model/Api2/Items/Rest.php
+++ app/code/local/Easyship/Shipping/Model/Api2/Items/Rest.php
@@ -3,7 +3,7 @@
  * Class Easyship_Shipping_Model_Api2_Items_Rest
  * Author: Easyship
  * Developer: Sunny Cheung, Holubiatnikova Anna, Aloha Chen, Phanarat Pak, Paul Lugangne Delpon
- * Version: 0.1.4
+ * Version: 0.1.5
  * Author URI: https://www.easyship.com
 */
 class Easyship_Shipping_Model_Api2_Items_Rest extends Easyship_Shipping_Model_Api2_Items
